90193a49421fc13cf0cc5d885cff0f253ca1a8ec
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-11 11:11:04 +0530

    Check50percentStake

--- Gambler/Program.cs
+++ Gambler/Program.cs
@@ -17,27 +17,35 @@ using System.Text;
             {
 
                 const int Stake = 100;
-                const int Bet = 1;
+                    const int Bet = 1;
+                    const int win = 1;
+                  const int loose = 0;
+                     int noOfbets = 0;
 
-                Gambler gambler1 = new Gambler(Stake);
+            Gambler gambler1 = new Gambler(Stake);
                 int Gamblers_Stake = gambler1.Gamblers_stake;
-
+            while (Gamblers_Stake > 50 && Gamblers_Stake < 150)
+            {
                 Random random = new Random();
                 int WinOrLoose = random.Next(0, 2);
                 switch (WinOrLoose)
                 {
                     case 0:
                         Gamblers_Stake = Gamblers_Stake - Bet;
+                        Console.WriteLine("Loose");
                         break;
                     case 1:
                         Gamblers_Stake = Gamblers_Stake + Bet;
+                        Console.WriteLine("Win");
                         break;
                 }
+                noOfbets++;
+            }
 
                 Console.WriteLine(Gamblers_Stake);
             Console.ReadKey();
 
-            }
+        }
         }
     }
 
